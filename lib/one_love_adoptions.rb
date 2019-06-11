01a2e41865e243bf0b50e1ce5cd2d259e96d298d
Katherine Jablonski <kjablonski08@gmail.com>
2019-06-11 22:50:18 +0000

    first commit

---
+++ lib/one_love_adoptions.rb
@@ -0,0 +1,6 @@
+require "one_love_adoptions/version"
+
+module OneLoveAdoptions
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/one_love_adoptions/version.rb
@@ -0,0 +1,3 @@
+module OneLoveAdoptions
+  VERSION = "0.1.0"
+end
